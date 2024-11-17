44a2feab5dc64cc5a9dceb9893e25a32b864d364
Rustam Sadreev <rustam17.sb@gmail.com>
2024-11-17 15:39:11 +0300

    feat: add 404 page component

---
+++ src/pages/not-found-page/not-found-page.tsx
@@ -0,0 +1,45 @@
+function NotFoundPage(): JSX.Element {
+  return (
+    <div className="page">
+      <header className="header">
+        <div className="container">
+          <div className="header__wrapper">
+            <div className="header__left">
+              <a className="header__logo-link" href="main.html">
+                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
+              </a>
+            </div>
+            <nav className="header__nav">
+              <ul className="header__nav-list">
+                <li className="header__nav-item user">
+                  <a className="header__nav-link header__nav-link--profile" href="#">
+                    <div className="header__avatar-wrapper user__avatar-wrapper">
+                    </div>
+                    <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
+                    <span className="header__favorite-count">3</span>
+                  </a>
+                </li>
+                <li className="header__nav-item">
+                  <a className="header__nav-link" href="#">
+                    <span className="header__signout">Sign out</span>
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </header>
+
+      <main className="page__main page__main--favorites">
+        <div className="page__favorites-container container">
+          <section className="favorites" style={{ padding: '100px 20px', textAlign: 'center' }}>
+            <h1 className="favorites__title">404 Not Found</h1>
+            <a className="form__submit button" href="main.html">Go to Homepage</a>
+          </section>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default NotFoundPage;
