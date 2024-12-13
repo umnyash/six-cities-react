2d19b73ac27592b783fdea5ed5d44783dc01f0e9
Rustam Sadreev <rustam17.sb@gmail.com>
2024-12-13 18:08:57 +0300

    feat: add a version of the main page without offers

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -6,6 +6,7 @@ import { OffersListVariant } from '../../types/offers-list-variant';
 import OffersList from '../../components/offers-list';
 import Logo from '../../components/logo';
 import Map from '../../components/map';
+import clsx from 'clsx';
 
 type MainPageProps = {
   offers: Offers;
@@ -14,6 +15,17 @@ type MainPageProps = {
 
 function MainPage({ offers, offersCount }: MainPageProps): JSX.Element {
   const setActiveCardId = useState('')[1];
+  const isEmpty = false;
+
+  const mainClassName = clsx(
+    'page__main page__main--index',
+    isEmpty && 'page__main--index-empty'
+  );
+
+  const containerClassName = clsx(
+    'cities__places-container container',
+    isEmpty && 'cities__places-container--empty'
+  );
 
   return (
     <div className="page page--gray page--main">
@@ -44,7 +56,7 @@ function MainPage({ offers, offersCount }: MainPageProps): JSX.Element {
         </div>
       </header>
 
-      <main className="page__main page__main--index">
+      <main className={mainClassName}>
         <h1 className="visually-hidden">Cities</h1>
         <div className="tabs">
           <section className="locations container">
@@ -83,33 +95,47 @@ function MainPage({ offers, offersCount }: MainPageProps): JSX.Element {
           </section>
         </div>
         <div className="cities">
-          <div className="cities__places-container container">
-            <section className="cities__places places">
-              <h2 className="visually-hidden">Places</h2>
-              <b className="places__found">{offersCount} places to stay in Amsterdam</b>
-              <form className="places__sorting" action="#" method="get">
-                <span className="places__sorting-caption">Sort by</span>{' '}
-                <span className="places__sorting-type" tabIndex={0}>
-                  Popular
-                  <svg className="places__sorting-arrow" width="7" height="4">
-                    <use xlinkHref="#icon-arrow-select"></use>
-                  </svg>
-                </span>
-                <ul className="places__options places__options--custom places__options--opened">
-                  <li className="places__option places__option--active" tabIndex={0}>Popular</li>
-                  <li className="places__option" tabIndex={0}>Price: low to high</li>
-                  <li className="places__option" tabIndex={0}>Price: high to low</li>
-                  <li className="places__option" tabIndex={0}>Top rated first</li>
-                </ul>
-              </form>
-              <OffersList offers={offers} variant={OffersListVariant.Rows} setActiveCardId={setActiveCardId} />
-            </section>
+          <div className={containerClassName}>
+            {isEmpty && (
+              <section className="cities__no-places">
+                <div className="cities__status-wrapper tabs__content">
+                  <b className="cities__status">No places to stay available</b>
+                  <p className="cities__status-description">We could not find any property available at the moment in Dusseldorf</p>
+                </div>
+              </section>
+            )}
+
+            {!isEmpty && (
+              <section className="cities__places places">
+                <h2 className="visually-hidden">Places</h2>
+                <b className="places__found">{offersCount} places to stay in Amsterdam</b>
+                <form className="places__sorting" action="#" method="get">
+                  <span className="places__sorting-caption">Sort by</span>{' '}
+                  <span className="places__sorting-type" tabIndex={0}>
+                    Popular
+                    <svg className="places__sorting-arrow" width="7" height="4">
+                      <use xlinkHref="#icon-arrow-select"></use>
+                    </svg>
+                  </span>
+                  <ul className="places__options places__options--custom places__options--opened">
+                    <li className="places__option places__option--active" tabIndex={0}>Popular</li>
+                    <li className="places__option" tabIndex={0}>Price: low to high</li>
+                    <li className="places__option" tabIndex={0}>Price: high to low</li>
+                    <li className="places__option" tabIndex={0}>Top rated first</li>
+                  </ul>
+                </form>
+                <OffersList offers={offers} variant={OffersListVariant.Rows} setActiveCardId={setActiveCardId} />
+              </section>
+            )}
+
             <div className="cities__right-section">
-              <Map
-                className="cities__map"
-                location={offers[0].city.location}
-                points={offers}
-              />
+              {!isEmpty && (
+                <Map
+                  className="cities__map"
+                  location={offers[0].city.location}
+                  points={offers}
+                />
+              )}
             </div>
           </div>
         </div>
