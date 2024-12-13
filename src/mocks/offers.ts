a80d90f40d05b35a826a700c7f6fbb82691c6785
Rustam Sadreev <rustam17.sb@gmail.com>
2024-12-13 18:21:06 +0300

    feat: update mock offers data

--- src/mocks/offers.ts
+++ src/mocks/offers.ts
@@ -94,4 +94,73 @@ export const offers: Offers = [
     isPremium: false,
     isFavorite: false,
   },
+  {
+    id: crypto.randomUUID(),
+    title: 'Waterfront with extraordinary view',
+    previewImage: 'img/apartment-01.jpg',
+    type: HousingType.Room,
+    rating: 4.7,
+    price: 209,
+    city: {
+      name: Cities[0],
+      location: {
+        latitude: 48.85661,
+        longitude: 2.351499,
+        zoom: 13
+      }
+    },
+    location: {
+      latitude: 48.858610000000006,
+      longitude: 2.330499,
+      zoom: 16
+    },
+    isPremium: false,
+    isFavorite: true,
+  },
+  {
+    id: crypto.randomUUID(),
+    title: 'The Joshua Tree House',
+    previewImage: 'img/apartment-02.jpg',
+    type: HousingType.Apartment,
+    rating: 3.9,
+    price: 458,
+    city: {
+      name: Cities[0],
+      location: {
+        latitude: 48.85661,
+        longitude: 2.351499,
+        zoom: 13
+      }
+    },
+    location: {
+      latitude: 48.834610000000005,
+      longitude: 2.335499,
+      zoom: 16
+    },
+    isPremium: false,
+    isFavorite: false,
+  },
+  {
+    id: crypto.randomUUID(),
+    title: 'Amazing and Extremely Central Flat',
+    previewImage: 'img/apartment-03.jpg',
+    type: HousingType.Hotel,
+    rating: 4.3,
+    price: 460,
+    city: {
+      name: Cities[2],
+      location: {
+        latitude: 50.846557,
+        longitude: 4.351697,
+        zoom: 13
+      }
+    },
+    location: {
+      latitude: 50.849557,
+      longitude: 4.374696999999999,
+      zoom: 16
+    },
+    isPremium: true,
+    isFavorite: false,
+  },
 ];
