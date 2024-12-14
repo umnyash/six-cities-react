2921a3aa892e0a59f669dff6979608b65f801b23
Rustam Sadreev <rustam17.sb@gmail.com>
2024-12-14 23:35:34 +0300

    feat: create store and connect redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './components/app';
 import { offers } from './mocks/offers';
 import { reviews } from './mocks/reviews';
+import { store } from './store';
 
 const OFFERS_COUNT = 312;
 
@@ -12,6 +14,8 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App offersCount={OFFERS_COUNT} offers={offers} reviews={reviews} />
+    <Provider store={store}>
+      <App offersCount={OFFERS_COUNT} offers={offers} reviews={reviews} />
+    </Provider>
   </React.StrictMode>
 );
