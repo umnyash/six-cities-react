a5edba6f3b744e20a6c0f76c5fe45b21454de2aa
Rustam Sadreev <rustam17.sb@gmail.com>
2025-01-22 22:29:36 +0300

    feat: add spinner component

---
+++ src/components/spinner/spinner.tsx
@@ -0,0 +1,15 @@
+import style from './spinner.module.css';
+
+function Spinner(): JSX.Element {
+  return (
+    <div className={style.spinner}>
+      <svg width="48" height="48" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <rect x="1" y="1" rx="1" width="10" height="10" fill='#4481c3' />
+        <rect x="1" y="1" rx="1" width="10" height="10" fill='#4481c3' />
+        <rect x="1" y="1" rx="1" width="10" height="10" fill='#4481c3' />
+      </svg>
+    </div>
+  );
+}
+
+export default Spinner;
