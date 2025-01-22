adb25b690698df85c7f7e0b2b9bcd28bf55e3ebd
Rustam Sadreev <rustam17.sb@gmail.com>
2025-01-22 17:36:37 +0300

    refactor: rename the action for setting offers in the store

--- src/store/action.ts
+++ src/store/action.ts
@@ -1,5 +1,5 @@
 import { createAction } from '@reduxjs/toolkit';
 import { Offers, CityName } from '../types/offers';
 
-export const setOffers = createAction<Offers>('catalog/setOffers');
+export const setOffers = createAction<Offers>('offers/set');
 export const setCity = createAction<CityName>('catalog/setCity');
