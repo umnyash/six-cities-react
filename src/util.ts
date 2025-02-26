a0dfd06ee2618c2a7f854843b99e43e7afa147dc
Rustam Sadreev <rustam17.sb@gmail.com>
2025-02-26 04:35:41 +0300

    feat: add groupBy util function

--- src/util.ts
+++ src/util.ts
@@ -22,3 +22,19 @@ export const getRandomArrayItems = <T>(array: T[], count: number): T[] => {
 
   return Array.from(set);
 };
+
+export const groupBy = <K extends PropertyKey, T>(items: Iterable<T>, getKey: (item: T) => K) =>
+  Array.from(items).reduce(
+    (result: Partial<Record<K, T[]>>, item) => {
+      const key = getKey(item);
+
+      if (!result[key]) {
+        result[key] = [];
+      }
+
+      result[key].push(item);
+
+      return result;
+    },
+    {}
+  );
