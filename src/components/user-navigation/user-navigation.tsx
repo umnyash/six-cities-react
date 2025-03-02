cce7cbfa3da77645a445f14505e30c4091e4cd84
Rustam Sadreev <rustam17.sb@gmail.com>
2025-03-02 03:55:04 +0300

    refactor: make the links to the login page and favorites pages separate components

--- src/components/user-navigation/user-navigation.tsx
+++ src/components/user-navigation/user-navigation.tsx
@@ -11,9 +11,6 @@ function UserNavigation(): JSX.Element {
   const authorizationStatus = useAppSelector(getAuthorizationStatus);
   const user = useAppSelector(getUser);
   const favorites = useAppSelector(getFavorites);
-  const isAuthorized = authorizationStatus === AuthorizationStatus.Auth;
-
-  const profileRoute = isAuthorized ? AppRoute.Favorites : AppRoute.Login;
 
   const dispatch = useAppDispatch();
 
@@ -25,21 +22,23 @@ function UserNavigation(): JSX.Element {
   return (
     <ul className="header__nav-list">
       <li className="header__nav-item user">
-        <Link className="header__nav-link header__nav-link--profile" to={profileRoute}>
-          <div className="header__avatar-wrapper user__avatar-wrapper" />
+        {authorizationStatus === AuthorizationStatus.Auth && user && (
+          <Link className="header__nav-link header__nav-link--profile" to={AppRoute.Favorites}>
+            <div className="header__avatar-wrapper user__avatar-wrapper" />
+            <span className="header__user-name user__name">{user.email}</span>
+            <span className="header__favorite-count">{favorites.length}</span>
+          </Link>
+        )}
 
-          {isAuthorized && user && (
-            <>
-              <span className="header__user-name user__name">{user.email}</span>
-              <span className="header__favorite-count">{favorites.length}</span>
-            </>
-          )}
-
-          {!isAuthorized && <span className="header__login">Sign in</span>}
-        </Link>
+        {authorizationStatus === AuthorizationStatus.NoAuth && (
+          <Link className="header__nav-link header__nav-link--profile" to={AppRoute.Login}>
+            <div className="header__avatar-wrapper user__avatar-wrapper" />
+            <span className="header__login">Sign in</span>
+          </Link>
+        )}
       </li>
 
-      {isAuthorized && (
+      {authorizationStatus === AuthorizationStatus.Auth && (
         <li className="header__nav-item">
           <a className="header__nav-link" href="#" onClick={handleLogoutButtonClick}>
             <span className="header__signout">Sign out</span>
