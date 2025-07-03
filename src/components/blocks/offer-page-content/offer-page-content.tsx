028c87558e4f78afbfd3f3dd7a33474dcf5d32c9
Rustam Sadreev <rustam17.sb@gmail.com>
2025-07-03 14:36:38 +0300

    refactor: add offer page content component

---
+++ src/components/blocks/offer-page-content/offer-page-content.tsx
@@ -0,0 +1,35 @@
+import { NEARBY_OFFERS_COUNT } from '../../../const';
+import { PageOffer } from '../../../types/offers';
+import useAppSelector from '../../../hooks/use-app-selector';
+import { getNearbyOffers, getNearbyOffersLoadingStatus } from '../../../store/nearby-offers/nearby-offers.selectors';
+import useArrayRandomIndices from '../../../hooks/use-array-random-indices';
+import Offer from '../offer';
+import Offers from '../offers';
+
+type OfferPageContentProps = {
+  offer: PageOffer;
+}
+
+function OfferPageContent({ offer }: OfferPageContentProps): JSX.Element {
+  const nearbyOffers = useAppSelector(getNearbyOffers);
+  const nearbyOffersLoadingStatus = useAppSelector(getNearbyOffersLoadingStatus);
+
+  const randomNearbyOffersIndices = useArrayRandomIndices(
+    nearbyOffers.length,
+    NEARBY_OFFERS_COUNT,
+    nearbyOffersLoadingStatus
+  );
+
+  const randomNearbyOffers = randomNearbyOffersIndices.map((index) => nearbyOffers[index]);
+
+  return (
+    <main className="page__main page__main--offer">
+      <Offer offer={offer} nearbyOffers={randomNearbyOffers} />
+      <div className="container">
+        <Offers heading="Other places in the neighbourhood" offers={randomNearbyOffers} />
+      </div>
+    </main>
+  );
+}
+
+export default OfferPageContent;
--- src/components/pages/offer-page/offer-page.tsx
+++ src/components/pages/offer-page/offer-page.tsx
@@ -1,18 +1,15 @@
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
-import { RequestStatus, NEARBY_OFFERS_COUNT } from '../../../const';
+import { RequestStatus } from '../../../const';
 import useAppDispatch from '../../../hooks/use-app-dispatch';
 import useAppSelector from '../../../hooks/use-app-selector';
-import useArrayRandomIndices from '../../../hooks/use-array-random-indices';
 import { fetchOffer, fetchReviews, fetchNearbyOffers } from '../../../store/async-actions';
 import { getOffer, getOfferLoadingStatus } from '../../../store/offer/offer.selectors';
-import { getNearbyOffers, getNearbyOffersLoadingStatus } from '../../../store/nearby-offers/nearby-offers.selectors';
 
 import LoadingPage from '../loading-page';
 import NotFoundPage from '../not-found-page';
-import Offer from '../../blocks/offer';
-import Offers from '../../blocks/offers';
+import OfferPageContent from '../../blocks/offer-page-content';
 
 function OfferPage(): JSX.Element {
   const offerId = useParams().id as string;
@@ -27,32 +24,12 @@ function OfferPage(): JSX.Element {
     dispatch(fetchNearbyOffers(offerId));
   }, [offerId, dispatch]);
 
-  const nearbyOffers = useAppSelector(getNearbyOffers);
-  const nearbyOffersLoadingStatus = useAppSelector(getNearbyOffersLoadingStatus);
-
-  const randomNearbyOffersIndices = useArrayRandomIndices(
-    nearbyOffers.length,
-    NEARBY_OFFERS_COUNT,
-    nearbyOffersLoadingStatus
-  );
-
-  const randomNearbyOffers = nearbyOffers.length
-    ? randomNearbyOffersIndices.map((index) => nearbyOffers[index])
-    : [];
-
   if (offerLoadingStatus === RequestStatus.Pending) {
     return <LoadingPage />;
   }
 
   if (offerLoadingStatus === RequestStatus.Success && offer) {
-    return (
-      <main className="page__main page__main--offer">
-        <Offer offer={offer} nearbyOffers={randomNearbyOffers} />
-        <div className="container">
-          <Offers heading="Other places in the neighbourhood" offers={randomNearbyOffers} />
-        </div>
-      </main>
-    );
+    return <OfferPageContent offer={offer} />;
   }
 
   return <NotFoundPage />;
